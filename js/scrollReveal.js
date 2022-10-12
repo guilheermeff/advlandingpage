2195b94a698db53bbe0947a0ef7a24f313d02d99
Guilherme Fernandes <guilheermeff@outlook.com>
2022-10-12 17:59:28 -0400

    start

---
+++ js/scrollReveal.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('header', {
+  delay: 800,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 0
+  }
+})
+
+ScrollReveal().reveal('main', {
+  delay: 800,
+  rotate: {
+    x: 100,
+    y: 100,
+    z: 0
+  }
+})
+
+ScrollReveal().reveal('footer', {
+  delay: 800,
+  rotate: {
+    x: 100,
+    y: 0,
+    z: 0
+  }
+})
